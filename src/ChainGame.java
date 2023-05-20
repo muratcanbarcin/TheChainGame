6bb61cfe0a0949b4675ea32e608daae820a90668
Murat Can Barçın <muratcanbarcin@gmail.com>
2023-05-20 03:47:15 +0300

    Added menu() function

--- src/ChainGame.java
+++ src/ChainGame.java
@@ -25,6 +25,82 @@ public class ChainGame {
 
     public ChainGame() throws Exception{  // --- Contructor
 
+        // ------ Standard code for keyboard ------ Do not change
+        klis=new KeyListener() {
+            public void keyTyped(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {
+                if(keypr==0) {
+                    keypr=1;
+                    rkey=e.getKeyCode();
+                }
+            }
+            public void keyReleased(KeyEvent e) {}
+        };
+        cn.getTextWindow().addKeyListener(klis);
+        // ----------------------------------------------------
+
+
+        menu();
     }
+
+    public void menu() {
+        int selectedOption = 1; // Seçili olan seçeneği tutan değişken
+        boolean menuSelected = false; // Menü seçildi mi?
+
+        while (!menuSelected) {
+            cn.getTextWindow().setCursorPosition(0, 0);
+            cn.getTextWindow().output("  Play");
+            cn.getTextWindow().setCursorPosition(0, 1);
+            cn.getTextWindow().output("  Settings");
+            cn.getTextWindow().setCursorPosition(0, 2);
+            cn.getTextWindow().output("  High Score Table");
+            cn.getTextWindow().setCursorPosition(0, 3);
+            cn.getTextWindow().output("  Exit");
+
+            cn.getTextWindow().setCursorPosition(0, selectedOption - 1);
+            cn.getTextWindow().output("->");
+
+            keypr = 0;
+            while (keypr == 0) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (rkey == KeyEvent.VK_DOWN) {
+                selectedOption++;
+                if (selectedOption > 4) {
+                    selectedOption = 1;
+                }
+            } else if (rkey == KeyEvent.VK_UP) {
+                selectedOption--;
+                if (selectedOption < 1) {
+                    selectedOption = 4;
+                }
+            } else if (rkey == KeyEvent.VK_ENTER) {
+                menuSelected = true;
+            }
+        }
+
+        // Seçilen seçenek üzerinde işlem yapılabilir
+        switch (selectedOption) {
+            case 1:
+                cn.getTextWindow().output("Play Seçildi");
+                // Play seçeneği seçildiğinde yapılacak işlemler
+                break;
+            case 2:
+                // Settings seçeneği seçildiğinde yapılacak işlemler
+                break;
+            case 3:
+                // High Score Table seçeneği seçildiğinde yapılacak işlemler
+                break;
+            case 4:
+                // Exit seçeneği seçildiğinde yapılacak işlemler
+                break;
+        }
+    }
+
 
 }
