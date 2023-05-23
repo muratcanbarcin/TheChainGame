517e9a6b5bdb1a2dd740c6b7185a3df52e3e33ef
Murat Can Barçın <muratcanbarcin@gmail.com>
2023-05-23 16:29:00 +0300

    add "addSorted()" method

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -3,6 +3,7 @@ public class DoubleLinkedList {
     private DLLNode tail;
     private int size;
 
+    /*
     public void add(Object data) {
         DLLNode newNode = new DLLNode(data);
 
@@ -17,7 +18,44 @@ public class DoubleLinkedList {
 
         size++;
     }
+*/
 
+    public void addSorted(String name, int score) {
+        DLLNode newNode = new DLLNode(name + " " + score);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else if (score >= Integer.parseInt(tail.getData().toString().split(" ")[1])) {
+            newNode.setPrev(tail);
+            tail.setNext(newNode);
+            tail = newNode;
+        } else if (score <= Integer.parseInt(head.getData().toString().split(" ")[1])) {
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+        } else {
+            DLLNode currentNode = head.getNext();
+
+            while (currentNode != null) {
+                int currentScore = Integer.parseInt(currentNode.getData().toString().split(" ")[1]);
+
+                if (score >= currentScore) {
+                    DLLNode prevNode = currentNode.getPrev();
+                    newNode.setPrev(prevNode);
+                    newNode.setNext(currentNode);
+                    prevNode.setNext(newNode);
+                    currentNode.setPrev(newNode);
+                    break;
+                }
+
+                currentNode = currentNode.getNext();
+            }
+        }
+
+        size++;
+    }
+    
     public int count() {
         return size;
     }
@@ -49,4 +87,7 @@ public class DoubleLinkedList {
             currentNode = currentNode.getNext();
         }
     }
+
+
+
 }
